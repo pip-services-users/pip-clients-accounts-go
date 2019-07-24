7e902afe3d6b88b9d028e2d3e09e2ab4f73b7da5
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2019-07-23 18:02:40 -0700

    Fixed constructor function for accounts

--- version1/AccountV1.go
+++ version1/AccountV1.go
@@ -5,38 +5,38 @@ import (
 )
 
 type AccountV1 struct {
-    /* Identification */
-    Id 			string	`json:"id"`
-    Login 		string	`json:"login"`
-    Name		string	`json:"name"`
+	/* Identification */
+	Id    string `json:"id"`
+	Login string `json:"login"`
+	Name  string `json:"name"`
 
-    /* Activity tracking */
-    CreateTime	time.Time `json:"create_time"`
-    Deleted		bool	`json:"deleted"`
-    Active		bool	`json:"active"`
+	/* Activity tracking */
+	CreateTime time.Time `json:"create_time"`
+	Deleted    bool      `json:"deleted"`
+	Active     bool      `json:"active"`
 
-    /* User preferences */
-    About		string	`json:"about"`
-    TimeZone	string	`json:"time_zone"`
-    Language	string	`json:"language"`
-    Theme		string	`json:"theme"`
+	/* User preferences */
+	About    string `json:"about"`
+	TimeZone string `json:"time_zone"`
+	Language string `json:"language"`
+	Theme    string `json:"theme"`
 
-    /* Custom fields */
-    CustomHdr 	interface{}	`json:"custom_hdr"`
-    CustomDat	interface{}	`json:"custom_dat"`
+	/* Custom fields */
+	CustomHdr interface{} `json:"custom_hdr"`
+	CustomDat interface{} `json:"custom_dat"`
 }
 
-func EmptyAccountV1(id string, login string, name string) *AccountV1 {
+func EmptyAccountV1() *AccountV1 {
 	return &AccountV1{}
 }
 
 func NewAccountV1(id string, login string, name string) *AccountV1 {
 	return &AccountV1{
-		Id: id,
-		Login: login,
-		Name: name,
+		Id:         id,
+		Login:      login,
+		Name:       name,
 		CreateTime: time.Now(),
-		Active: true,
-		Deleted: false,
+		Active:     true,
+		Deleted:    false,
 	}
 }
