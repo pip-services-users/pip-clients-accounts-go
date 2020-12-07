8a484856f5b01f6b7079d674665abb205b3adca3
nikopol <levichev.dmitry@gmail.com>
2020-12-07 11:56:19 +0300

    Add factory

---
+++ build/AccountsClientFactory.go
@@ -0,0 +1,31 @@
+package build
+
+import (
+	clients1 "github.com/pip-services-users/pip-clients-accounts-go/version1"
+	cref "github.com/pip-services3-go/pip-services3-commons-go/refer"
+	cbuild "github.com/pip-services3-go/pip-services3-components-go/build"
+)
+
+type AccountsClientFactory struct {
+	cbuild.Factory
+}
+
+func NewAccountsClientFactory() *AccountsClientFactory {
+	c := &AccountsClientFactory{
+		Factory: *cbuild.NewFactory(),
+	}
+
+	// nullClientDescriptor := cref.NewDescriptor("pip-services-sasswords", "client", "null", "*", "1.0")
+	// directClientDescriptor := cref.NewDescriptor("pip-services-sasswords", "client", "direct", "*", "1.0")
+	memoryClientDescriptor := cref.NewDescriptor("pip-services-accounts", "client", "memory", "*", "1.0")
+	cmdHttpClientDescriptor := cref.NewDescriptor("pip-services-accounts", "client", "commandable-http", "*", "1.0")
+	grpcClientDescriptor := cref.NewDescriptor("pip-services-accounts", "client", "grpc", "*", "1.0")
+
+	// c.RegisterType(nullClientDescriptor, clients1.NewAccountsNullClientV1)
+	// c.RegisterType(directClientDescriptor, clients1.NewAccountsDirectClientV1)
+	c.RegisterType(memoryClientDescriptor, clients1.NewAccountsMemoryClientV1)
+	c.RegisterType(cmdHttpClientDescriptor, clients1.NewAccountsHttpCommandableClientV1)
+	c.RegisterType(grpcClientDescriptor, clients1.NewAccountGrpcClientV1)
+
+	return c
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.3.2
 	github.com/pip-services-users/pip-clients-sessions-go v0.0.0-20201204094920-ef490c330a72
 	github.com/pip-services3-go/pip-services3-commons-go v1.0.3
+	github.com/pip-services3-go/pip-services3-components-go v1.0.4
 	github.com/pip-services3-go/pip-services3-data-go v1.0.6
 	github.com/pip-services3-go/pip-services3-grpc-go v0.0.0-20190804070827-816edc2ee7e3
 	github.com/pip-services3-go/pip-services3-rpc-go v1.0.11
